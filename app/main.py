e3cbed45ac6c85f373751caff4d1cfb13209b91a
khido <mcdonald.am97@gmail.com>
2022-08-06 00:09:01 +0100

    Add predictor function

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+import joblib
 
 app = FastAPI()
+model = joblib.load("app/models/sota_model.joblib")
 
 @app.get("/")
 def read_root():
     return {"API status": "Hello World!"}
+
+
+@app.post("/predict/")
+def predict_price(
+    location: str = Query(..., description="Location", min_length=1),
+    size: float = Query(..., description="Size (in m²)", ge=10.0),
+    bedrooms: int = Query(2, description="N° bedrooms", ge=0, le=50)
+):
+    example = [location, size, bedrooms]
+    price = model.predict([example])[0]
+    return {"prediction": int(price)}
